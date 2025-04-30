84ca48f008d32fd67af3ba3fa7ff60ccc280ab16
MIYAKE Ren <miyake.ren.i9@s.gifu-u.ac.jp>
2025-04-30 12:16:42 +0900

    Added Finish Button

--- app/src/main/java/jp/ac/gifu_u/s/miyake_ren/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/s/miyake_ren/myapplication/MainActivity.java
@@ -1,24 +1,38 @@
 package jp.ac.gifu_u.s.miyake_ren.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button_finish1);
+        b.setOnClickListener(this);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "アプリを終了しました", Toast.LENGTH_SHORT).show();
+
+        finish();
     }
 }
