a7baa6ed0268005ca2fdad948a1063cd5e81595d
MIYAKE Ren <miyake.ren.i9@s.gifu-u.ac.jp>
2025-05-21 11:08:02 +0900

    Changed Content : CameraApp

--- app/src/main/java/jp/ac/gifu_u/s/miyake_ren/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/s/miyake_ren/myapplication/MainActivity.java
@@ -1,24 +1,22 @@
 package jp.ac.gifu_u.s.miyake_ren.myapplication;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // タイトルバー非表示 & 全画面
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // カメラプレビューをそのまま全画面に設定
+        setContentView(new CameraView(this));
     }
 }
